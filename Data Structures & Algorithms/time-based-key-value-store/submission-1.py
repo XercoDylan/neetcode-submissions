0420deb1093258598adbc319204aac8a6488738b
Xerco Tchouankeu <xerco@mit.edu>
2026-08-01 10:48:14 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,38 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keys = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.keys:
+            self.keys[key].append((timestamp, value))
+        else:
+            self.keys[key] = [(timestamp, value)]
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not key in self.keys:
+            return ""
+        
+        search_list = self.keys[key]
+        best = (-float("inf"), "")
+
+        low = 0
+        high = len(search_list) - 1
+
+        while (low <= high):
+            mid = low + (high - low)//2
+
+            current_timestamp, current_value = search_list[mid]
+
+            if current_timestamp <= timestamp:
+                if current_timestamp > best[0]:
+                    best = (current_timestamp, current_value)
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        
+        return best[1]
+        
